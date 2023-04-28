6f639473d24d60040e4513a2841da4c1bfad4489
junaid-o <junaidmohd795@gmail.com>
2023-04-28 13:59:00 +0530

    PerformanceEvaluation

--- ThyroidPrediction/component/model_performance.py
+++ ThyroidPrediction/component/model_performance.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_curve, auc
-
+from sklearn.model_selection import learning_curve
 
 class ModelPerformance:
 
@@ -113,7 +113,7 @@ class ModelPerformance:
 
             #################################################
             fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(6, 6), squeeze=True)
-            fig.suptitle('Confusion Metrix: Oversampled Data with Major Classes', fontsize=22, fontweight='bold', y=0.98)
+            fig.suptitle('Confusion Metrix: Oversampled Data with Major Classes', fontsize=20, fontweight='bold', y=0.98)
             # Uncomment the axes line if increasing column in above line of code
             try:
                 # flattening is required only when nrows or ncols is mor than one
@@ -150,7 +150,7 @@ class ModelPerformance:
             #plt.savefig(f'Results/Results_Classification_resampled/ConfusionMetrix_resampled/ConfusionMetrix_resampled.svg',format='svg',dpi=600)
 
             os.makedirs(self.model_performance_dir,exist_ok=True)
-            plt.savefig(os.path.join(self.model_performance_dir, "confusion_metrix.svg"), format="svg", bbox_inches='tight', dpi=300)
+            plt.savefig(os.path.join(self.model_performance_dir, "confusion_metrix.svg"), format="svg", bbox_inches='tight', dpi=500)
 
             #######################################################################
             #plt.show()
@@ -161,7 +161,7 @@ class ModelPerformance:
 
     def get_roc_auc_curve(self):
         try:
-            train_dataframe, X_test, train_target_arr, y_test, RF_model = self.get_trained_model_and_data()
+            _, X_test, _, y_test, RF_model = self.get_trained_model_and_data()
             
             # Compute the predicted probabilities for each class
             y_score = RF_model.predict_proba(X_test)
@@ -198,7 +198,7 @@ class ModelPerformance:
             #plt.savefig(f'Results/Results_Classification_resampled/ROC_AUC_OVR_Curve_resampled/ROC_AUC_OVR_Curve_resampled.svg',format='svg',dpi=600)
 
             roc_auc_curve_file_path = os.path.join(self.model_performance_dir,"roc_auc.svg")
-            plt.savefig(roc_auc_curve_file_path, format="svg", bbox_inches='tight', dpi=300)
+            plt.savefig(roc_auc_curve_file_path, format="svg", bbox_inches='tight', dpi=500)
             ##########################
             #plt.show()
 
@@ -206,9 +206,170 @@ class ModelPerformance:
             raise ThyroidException(e, sys) from e
         
 
-    def get_learning_curve_complexity_analysis(self):
+    def get_learning_curve(self):
         try:
-            pass
+            X_train, _, y_train, _, RF_model = self.get_trained_model_and_data()
+            ############## LEARNING CURVE: Subplot Canvas #####################
+
+            fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(6, 6), squeeze=True)
+            fig.suptitle('Learning Curve: Resampled Data With Major Class', fontsize=20, fontweight='bold', y=1.0)
+            
+            try:
+                # Needed Only when more than 1 columns or rows are need in above line of code
+                axes = axes.flatten()
+            except:
+                pass
+
+            i = 0
+            
+            #############  Getting Scores For Learning Curve  ###########
+
+            #model = {"AdaBoost":AdaBoost_model, "KNN":knn_model,"RF":RF_model}
+            model = {"RF":RF_model}
+
+            for name, clf in model.items():
+                print("\nFor ",name)
+                #parameters = param_grid[name]
+                #gs = grid_search(clf,parameters)
+
+                try:
+                    # Needed only when mor than 1 os or columns have been defined as subplots
+                    ax = axes[i]
+                except:
+                    ax = axes
+
+                i +=1
+            
+                # split dataset into training and test data
+                train_sizes, train_scores, test_scores, fit_time, score_time = learning_curve(clf, X_train, y_train,
+                                                                        cv=2,
+                                                                        verbose=3,
+                                                                        random_state= 2023,
+                                                                        #scoring=scoring,
+                                                                        train_sizes=np.linspace(0.1, 1.0, 3),
+                                                                        return_times= True
+                                                                    )
+                
+                ################# LEARNING CURVE PLOT AND EXPORT  ###############
+
+                # calculate mean and standard deviation of training and test scores
+                train_scores_mean = np.mean(train_scores, axis=1)
+                train_scores_std = np.std(train_scores, axis=1)
+                test_scores_mean = np.mean(test_scores, axis=1)
+                test_scores_std = np.std(test_scores, axis=1)
+
+                # plot learning curve
+                #plt.figure()
+                ax.set_title(f'Learning Curve: {name}')
+                ax.set_xlabel('Training examples')
+                ax.set_ylabel('Score')
+                ax.grid()
+                ax.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1, color='r')
+                ax.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color='g')
+                ax.plot(train_sizes, train_scores_mean, 'o-', color='r', label='Training score')
+                ax.plot(train_sizes, test_scores_mean, 'o-', color='g', label='Cross-validation score')
+                
+                ax.legend(loc='best')
+
+            plt.tight_layout()    
+                
+            ##################   EXPORTING FIGURE    #####################
+
+            #os.makedirs(f"Results/Results_Classification/LearningCurve_resampled", exist_ok=True)
+            
+            learning_curve_file_path = os.path.join(self.model_performance_dir, "learning_curve.svg")
+            plt.savefig(learning_curve_file_path, format='svg',dpi=500)
+            
+            #plt.show()
+            return train_sizes, train_scores, test_scores, fit_time, score_time
+
+        except Exception as e:
+            raise ThyroidException(e, sys) from e
+        
+
+    def get_scalability_performance(self):
+        try:
+            X_train, _, y_train, _, RF_model = self.get_trained_model_and_data()
+            #################################################
+            fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4), squeeze=True)
+            fig.suptitle(' Scalability & Performance: Resampled Data With Major Class', fontsize=22, fontweight='bold', y=1.0)
+            axes = axes.flatten()
+            i = 0
+            ###################################################
+
+
+            #for name, clf in {'SVM':svm_model, "AdaBoost":AdaBoost_model, "GBDT":GBDT_model,"KNN":knn_model,"LogReg":lr_model,"RF":RF_model}.items():
+            model = {"RF":RF_model}
+
+
+            for name, clf in model.items():
+                print("\nFor ",name)
+                
+                ax = axes[i]
+                ax2 = axes[i+1]
+                ax3 = axes[i+2]
+                i +=3
+            
+                # split dataset into training and test data
+                train_sizes, train_scores, test_scores, fit_time, score_time = learning_curve(clf, X_train, y_train,
+                                                                        cv=2,
+                                                                        verbose=2,
+                                                                        random_state=100,
+                                                                        #scoring=scoring,
+                                                                        train_sizes=np.linspace(0.1, 1.0, 3),
+                                                                    return_times=True)
+                
+                # calculate mean and standard deviation of training and test scores
+                fit_time_mean = np.mean(fit_time, axis=1)
+                fit_time_std = np.std(fit_time, axis=1)
+                
+                score_time_mean = np.mean(score_time, axis=1)
+                score_time_std = np.std(score_time, axis=1)
+                test_scores_mean = np.mean(test_scores, axis=1)
+                test_scores_std = np.std(test_scores, axis=1)
+                
+
+                # plot learning curve
+                #plt.figure()
+                ax.set_title(f'Scalability For: {name}')
+                ax2.set_title(f'Scalability For: {name}')
+                ax3.set_title(f'Performance of: {name}')
+                
+                ax.set_xlabel('Training examples')
+                ax2.set_xlabel('Training examples')
+                ax3.set_xlabel('Fit Time')
+                
+                ax.set_ylabel('Fit Time')
+                ax2.set_ylabel('Score Time')
+                ax3.set_ylabel('Test Score')
+                
+                ax.grid()
+                ax2.grid()
+                ax3.grid()
+                
+                ax.fill_between(train_sizes, fit_time_mean - fit_time_std, fit_time_mean + fit_time_std, alpha=0.1, color='r')
+                ax2.fill_between(train_sizes, score_time_mean - score_time_std, score_time_mean + score_time_std, alpha=0.1, color='g')
+                ax3.fill_between(fit_time_mean, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color='b')
+                
+                ax.plot(train_sizes, fit_time_mean, 'o-', color='r', label='Fit Time')
+                ax2.plot(train_sizes, score_time_mean, 'o-', color='g', label='Score Time')
+                ax3.plot(fit_time_mean, test_scores_mean, 'o-', color='b', label='Fit Time vs score')
+                
+                #ax.legend(loc='best')
+                #ax2.legend(loc='best')
+
+            plt.tight_layout()    
+                
+            #################### EXPORTING COMPLEXITY CURVES: EXPORT ########################
+
+            #os.makedirs(f'Results/Results_Classification_resampled/ScalabilityPerformance_resampled', exist_ok=True)
+            #plt.savefig(f'Results/Results_Classification_resampled/ScalabilityPerformance_resampled/ScalabilityPerformance_resampled.svg',format='svg',dpi=600)
+
+            scalability_curves_file_path = os.path.join(self.model_performance_dir,"scalability_performace.svg")
+            plt.savefig(scalability_curves_file_path,format='svg',dpi=600)
+                
+            #plt.show()
+
         except Exception as e:
             raise ThyroidException(e, sys) from e
         
@@ -217,6 +378,8 @@ class ModelPerformance:
         try:
             self.get_trained_model_and_data()
             self.get_confusion_metrix()
-            return self.get_roc_auc_curve()
+            self.get_roc_auc_curve()
+            self.get_learning_curve()
+            return self.get_scalability_performance()
         except Exception as e:
             raise ThyroidException(e, sys) from e
