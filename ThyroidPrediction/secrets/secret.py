65f147bd9facbf41e4ad8cd06b076ed55dda15f1
junaid-o <junaidmohd795@gmail.com>
2023-04-29 18:15:07 +0530

    S3 Bucket Implemented

--- ThyroidPrediction/secrets/secret.py
+++
@@ -1,8 +0,0 @@
-AWS_S3_ACCESS_KEY_ID = "AKIA3KSQY3GBZRDONHMO"
-AWS_S3_SECRET_ACCESS_KEY = "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"
-AWS_REGION = "ap-south-1"
-
-
-# Define the bucket and folder name
-AWS_BUCKET_NAME = 'my-bucket-101'
-AWS_FOLDER_NAME = 'raw_data/'
